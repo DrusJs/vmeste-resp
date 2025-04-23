ddb7a411f476e644229a30cc30af9c0b40c3abf2
“drusjs” <magnojoky962@gmail.com>
2025-04-23 10:11:08 +0300

    fix 1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const burgerMenu = document.querySelector('#burger-menu')
-const burgerButton = document.querySelector('#burger-button')
+const burgerMenu = document.querySelector('.burger-menu')
+const burgerButton = document.querySelector('.burger-button')
 
 if (burgerMenu && burgerButton) {
     burgerButton.addEventListener('click', ()=>{
@@ -13,8 +13,6 @@ if (burgerMenu && burgerButton) {
     })
 }
 
-
-// 1240
 const swiperMain = new Swiper(".support-section__swiper", {
     slidesPerView: 1,
     centeredSlides: true,
@@ -28,10 +26,12 @@ const swiperMain = new Swiper(".support-section__swiper", {
         1240: {
             slidesPerView: 3,
             centeredSlides: true,
+            spaceBetween: 80,
         },
         850: {
             slidesPerView: 2,
             centeredSlides: false,
+            spaceBetween: 80,
         }
     }
 });
@@ -57,3 +57,27 @@ const swiperNews = new Swiper(".news-section__swiper", {
         }
     }
 });
+
+
+const supportCont = document.querySelector('#support-tabs')
+
+if (supportCont) {
+    const supportTabButtons = Array.from(supportCont.children)
+    const supportTabItems = Array.from(document.querySelector('.tabs-sec').children)
+    
+    supportTabButtons.forEach(el=>{
+        el.addEventListener('click', ()=>{
+            if (!el.classList.contains('tab-button--active')) {
+                const active = document.querySelector('.tab-button--active')
+                const tabIndex = supportTabButtons.indexOf(el)
+
+                if (active) { active.classList.remove('tab-button--active') }
+                el.classList.add('tab-button--active')
+
+                supportTabItems.forEach(tab => { tab.style.display = "none" })
+                supportTabItems[tabIndex].style.display = "flex"
+            }   
+        })
+    })
+    
+}
